2a4c7276302cea57cf68164f9b804c826828395a
jaeseo0214 <jaeseoyun9891@gmail.com>
2025-11-02 14:19:38 +0900

    UserAnswer Update2

--- src/main/java/org/example/entity/UserAnswer.java
+++ src/main/java/org/example/entity/UserAnswer.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToOne;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
